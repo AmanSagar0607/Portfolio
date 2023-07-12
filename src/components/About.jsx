e98e723bf597f8819bb3b8e6e19c2cb51b86aeaa
Aman Sagar <62849475+AmanSagar0607@users.noreply.github.com>
2023-07-13 03:19:57 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
                 variants={fadeIn("", "", 0.1, 1)}
                 className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
             >
-                I'm a skilled software developer with experience in TypeScript and
+                I'm a skilled software developer with experience in HTML, CSS and
                 JavaScript, and expertise in frameworks like React, Node.js, and
                 Three.js. I'm a quick learner and collaborate closely with clients to
                 create efficient, scalable, and user-friendly solutions that solve
