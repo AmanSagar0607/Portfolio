ac7a18e3319caa12813b86b23543966b5ac232ed
Aman Sagar <luckysagar6265@gmail.com>
2024-02-05 06:12:33 +0530

    undo Resume

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 import Footer from './Footer';
+import Resume from './Resume';
 
 export {
   Hero,
@@ -22,5 +23,6 @@ export {
   BallCanvas, 
   ComputersCanvas, 
   StarsCanvas,
+  Resume,
   Footer
 }
