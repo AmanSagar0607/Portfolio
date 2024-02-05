d972083ce16085fd5c3895ffec9f1bc8d916c37f
Aman Sagar <luckysagar6265@gmail.com>
2024-02-05 06:00:46 +0530

    undo shopify png

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,7 +22,6 @@ import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
@@ -53,7 +52,6 @@ export {
   typescript,
   threejs,
   meta,
-  shopify,
   starbucks,
   tesla,
   carrent,
