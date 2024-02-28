daba7e0a07a74bf6413ecbd74f3d62fd6595e2e9
Aman Sagar <62849475+AmanSagar0607@users.noreply.github.com>
2024-02-29 00:32:40 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,7 @@ import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 import Footer from './Footer';
-import Resume from './Resume';
+import Resume from './Resume.jsx';
 
 export {
   Hero,
